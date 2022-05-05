dae0a816b0fdacdbd9d09cd0dda643e4addd22ae
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-05-05 17:14:02 +0200

    Added worksheet for production lines

---
+++ productionCalculatorLib/components/Worksheet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace productionCalculatorLib.components;
+
+public class Worksheet
+{
+    public string Name { get; set; } = "";
+    
+    private readonly List<Node> _nodes = new();
+
+    public IList<Node> Nodes => new ReadOnlyCollection<Node>(_nodes);
+
+    public void AddNode(Node node)
+    {
+        if (!_nodes.Contains(node))
+        {
+            _nodes.Add(node);
+        }
+    }
+
+    public void RemoveNode(Node node)
+    {
+        _nodes.Remove(node);
+        foreach (var n in _nodes)
+        {
+            n.RemoveConnectedNode(node);
+        }
+    }
+
+    public Worksheet()
+    {
+    }
+    
+    
+}
