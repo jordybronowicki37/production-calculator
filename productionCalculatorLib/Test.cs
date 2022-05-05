7b58bad0f447b21e15bf4f824da7df325b52b26c
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-05-05 17:11:45 +0200

    Added test main runnable

---
+++ productionCalculatorLib/Test.cs
@@ -0,0 +1,13 @@
+﻿using productionCalculatorLib.components;
+
+namespace productionCalculatorLib;
+
+public class Test
+{
+    public static void Main(string[] args)
+    {
+        var worksheet = new Worksheet();
+        
+        
+    }
+}
