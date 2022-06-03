5270dde211cdb9060f048da4dcfc91a26f420719
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-06-03 15:21:04 +0200

    Added react template site

---
+++ SiteReact/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteReact.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly ILogger<ProductController> _logger;
+    
+    public ProductController(ILogger<ProductController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+    
+    
+}
