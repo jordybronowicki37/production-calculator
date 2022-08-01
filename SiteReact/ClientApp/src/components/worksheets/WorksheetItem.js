34d317c5ce7dac68a48080bcc1c9ec1652ff45b4
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-08-01 16:21:46 +0200

    Added beginning of worksheet overview

--- SiteReact/ClientApp/src/components/worksheets/WorksheetItem.js
+++ SiteReact/ClientApp/src/components/worksheets/WorksheetItem.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
+import "./WorksheetItem.css";
 
 export class WorksheetItem extends Component {
+  constructor(props) {
+    super(props);
+    const {name} = props.data;
+    this.state = {
+      id: props.id,
+      name,
+    }
+  }
+
   render () {
     return (
-      <div>
-        WorksheetItem
+      <div className="worksheetItemContainer">
+        <Link className="worksheetItemTitle" to={`calculator/${this.state.id}`}>{this.state.name}</Link>
       </div>
     );
   }
--- SiteReact/ClientApp/src/components/worksheets/WorksheetOverview.js
+++ SiteReact/ClientApp/src/components/worksheets/WorksheetOverview.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import {WorksheetItem} from "./WorksheetItem";
+import {fetchAllWorksheets} from "./WorksheetAPI";
+import Store from "../../dataStore/DataStore";
+import "./WorksheetOverview.css";
 
 export class WorksheetOverview extends Component {
+  unsubscribe;
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      worksheets: []
+    }
+    this.unsubscribe = Store.subscribe(() => {
+      this.setState({worksheets: Store.getState().worksheets});
+      console.log(this.state)
+    });
+    fetchAllWorksheets();
+  }
   
   render () {
     return (
       <div>
         <h1>Worksheet overview</h1>
-        <WorksheetItem></WorksheetItem>
+        <div>
+          {this.state.worksheets.map((v, i) => <div className="worksheetItem"><WorksheetItem key={v.name} data={v} id={i}></WorksheetItem></div>)}
+        </div>
       </div>
     );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 }
