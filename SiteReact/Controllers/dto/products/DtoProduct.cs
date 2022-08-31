aa8c6679ec291b57705f462d23df13d96775b303
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-08-31 18:29:49 +0200

    Added constructor to product dto

--- SiteReact/Controllers/dto/products/DtoProduct.cs
+++ SiteReact/Controllers/dto/products/DtoProduct.cs
@@ -1,6 +1,13 @@
-﻿namespace SiteReact.Controllers.dto.products;
+﻿using productionCalculatorLib.components.products;
+
+namespace SiteReact.Controllers.dto.products;
 
 public class DtoProduct
 {
     public string Name { get; set; } = "";
+
+    public DtoProduct(Product product)
+    {
+        Name = product.Name;
+    }
 }
