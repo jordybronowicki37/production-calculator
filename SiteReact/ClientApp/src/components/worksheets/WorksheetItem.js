bf1d799c8658327e97ca212bef80fb9fdf1ebcae
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-08-31 22:46:01 +0200

    Added more info to worksheet item component

--- SiteReact/ClientApp/src/components/worksheets/WorksheetItem.js
+++ SiteReact/ClientApp/src/components/worksheets/WorksheetItem.js
@@ -5,17 +5,52 @@ import "./WorksheetItem.css";
 export class WorksheetItem extends Component {
   constructor(props) {
     super(props);
-    const {name} = props.data;
+    const {name, amountProducts, amountRecipes, amountNodes, inputProducts, outputProducts} = props.data;
+    
     this.state = {
-      id: props.id,
-      name,
+      id: props.id, 
+      name, amountProducts, amountRecipes, amountNodes, inputProducts, outputProducts,
     }
   }
 
   render () {
+    const {id, name, amountProducts, amountRecipes, amountNodes, inputProducts, outputProducts} = this.state;
+    
+    let inputs = inputProducts.map(v => 
+      <li key={v.product.name} className="worksheet-item-throughput">
+        <div>{v.product.name}</div>
+        <div>{v.amount}</div>
+      </li>);
+    let outputs = outputProducts.map(v =>
+      <li key={v.product.name} className="worksheet-item-throughput">
+        <div>{v.product.name}</div>
+        <div>{v.amount}</div>
+      </li>);
+    
     return (
-      <div className="worksheetItemContainer">
-        <Link className="worksheetItemTitle" to={`calculator/${this.state.id}`}>{this.state.name}</Link>
+      <div className="worksheet-item-container">
+        <div className="worksheet-item-top">
+          <Link className="worksheet-item-title" to={`calculator/${id}`}>{name}</Link>
+          <Link className="worksheet-item-open-icon" to={`calculator/${id}`}><i className='bx bx-folder-open bx-tada-hover'></i></Link>
+        </div>
+        <div className="worksheet-item-amounts">
+          <div>Products:</div>
+          <div>{amountProducts}</div>
+          <div>Recipes:</div>
+          <div>{amountRecipes}</div>
+          <div>Nodes:</div>
+          <div>{amountNodes}</div>
+        </div>
+        <div className="worksheet-item-io" hidden={inputProducts.length===0 && outputProducts.length===0}>
+          <div>Inputs</div>
+          <div>Outputs</div>
+          <ul>
+            {inputs}
+          </ul>
+          <ul>
+            {outputs}
+          </ul>
+        </div>
       </div>
     );
   }
