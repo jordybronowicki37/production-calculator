d60ce4d2f0e705f165cc9c6468ce86969f9e152d
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-08-31 18:31:15 +0200

    Added more properties to small worksheet dto

--- SiteReact/Controllers/dto/worksheets/DtoWorksheetSmall.cs
+++ SiteReact/Controllers/dto/worksheets/DtoWorksheetSmall.cs
@@ -1,13 +1,53 @@
-﻿using productionCalculatorLib.components.worksheet;
+﻿using productionCalculatorLib.components.nodes.nodeTypes;
+using productionCalculatorLib.components.worksheet;
+using SiteReact.Controllers.dto.throughputs;
 
 namespace SiteReact.Controllers.dto.worksheets;
 
 public class DtoWorksheetSmall
 {
     public string Name { get; }
-    
+    public int AmountProducts { get; }
+    public int AmountRecipes { get; }
+    public int AmountNodes { get; }
+
+    public List<DtoThroughPut> InputProducts { get; } = new();
+    public List<DtoThroughPut> OutputProducts { get; } = new();
+
     public DtoWorksheetSmall(Worksheet worksheet)
     {
         Name = worksheet.Name;
+        AmountProducts = worksheet.Products.Count;
+        AmountRecipes = worksheet.Recipes.Count;
+        AmountNodes = worksheet.Nodes.Count;
+
+        foreach (var node in worksheet.Nodes)
+        {
+            switch (node)
+            {
+                case SpawnNode spawnNode:
+                    var foundInput = InputProducts.Find(put => put.Product.Name == spawnNode.Product.Name);
+                    if (foundInput != null)
+                    {
+                        foundInput.Amount += spawnNode.Amount;
+                    } 
+                    else
+                    {
+                        InputProducts.Add(new DtoThroughPut(spawnNode.Product, spawnNode.Amount));
+                    }
+                    break;
+                case EndNode endNode:
+                    var foundOutput = InputProducts.Find(put => put.Product.Name == endNode.Product.Name);
+                    if (foundOutput != null)
+                    {
+                        foundOutput.Amount += endNode.Amount;
+                    } 
+                    else
+                    {
+                        OutputProducts.Add(new DtoThroughPut(endNode.Product, endNode.Amount));
+                    }
+                    break;
+            }
+        }
     }
 }
