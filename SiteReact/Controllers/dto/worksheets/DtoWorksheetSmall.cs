23407121d7b955d7599a9ce37d8ae0ce6ba4dcec
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-09-03 13:45:57 +0200

    Added id to small worksheet dto

--- SiteReact/Controllers/dto/worksheets/DtoWorksheetSmall.cs
+++ SiteReact/Controllers/dto/worksheets/DtoWorksheetSmall.cs
@@ -6,6 +6,7 @@ namespace SiteReact.Controllers.dto.worksheets;
 
 public class DtoWorksheetSmall
 {
+    public long Id { get; }
     public string Name { get; }
     public int AmountProducts { get; }
     public int AmountRecipes { get; }
@@ -16,6 +17,7 @@ public class DtoWorksheetSmall
 
     public DtoWorksheetSmall(Worksheet worksheet)
     {
+        Id = worksheet.Id;
         Name = worksheet.Name;
         AmountProducts = worksheet.Products.Count;
         AmountRecipes = worksheet.Recipes.Count;
