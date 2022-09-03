3379d3e7be7198879d46eff494fc4e56192c7122
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-09-03 14:26:36 +0200

    Added worksheet creator to worksheet overview as a popup

--- SiteReact/ClientApp/src/components/worksheets/WorksheetOverview.js
+++ SiteReact/ClientApp/src/components/worksheets/WorksheetOverview.js
@@ -3,6 +3,7 @@ import {WorksheetItem} from "./WorksheetItem";
 import {fetchAllWorksheets} from "./WorksheetAPI";
 import Store from "../../dataStore/DataStore";
 import "./WorksheetOverview.css";
+import {WorksheetCreator} from "./WorksheetCreator";
 
 export class WorksheetOverview extends Component {
   unsubscribe;
@@ -10,21 +11,33 @@ export class WorksheetOverview extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      worksheets: []
+      worksheets: [],
+      worksheetCreatorOpen:false,
     }
     this.unsubscribe = Store.subscribe(() => {
       this.setState({worksheets: Store.getState().worksheets});
-      console.log(this.state)
     });
     fetchAllWorksheets();
   }
   
   render () {
     return (
-      <div>
-        <h1>Worksheet overview</h1>
+      <div className="worksheet-overview">
+        <div hidden={!this.state.worksheetCreatorOpen} className="popup-container" onClick={() => this.setState({worksheetCreatorOpen:false})}>
+          <div onClick={e => e.stopPropagation()}>
+            <button type="button" className="popup-close-button" onClick={() => this.setState({worksheetCreatorOpen:false})}>
+              <i className='bx bx-x'></i>
+            </button>
+            <WorksheetCreator></WorksheetCreator>
+          </div>
+        </div>
+        
         <div>
-          {this.state.worksheets.map((v, i) => <div key={v.name} className="worksheetItem"><WorksheetItem data={v} id={i}></WorksheetItem></div>)}
+          <h1>Worksheet overview</h1>
+          <button type="button" onClick={() => this.setState({worksheetCreatorOpen:true})}>Create new</button>
+          <div>
+            {this.state.worksheets.map(v => <div key={v.name} className="worksheetItem"><WorksheetItem data={v} id={v.id}></WorksheetItem></div>)}
+          </div>
         </div>
       </div>
     );
