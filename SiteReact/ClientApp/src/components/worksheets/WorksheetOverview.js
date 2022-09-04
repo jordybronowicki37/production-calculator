8bd3557b5a3b559984c7b5fca7ee2552b035bf50
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-09-04 20:34:52 +0200

    Added filter to worksheet overview

--- SiteReact/ClientApp/src/components/worksheets/WorksheetOverview.js
+++ SiteReact/ClientApp/src/components/worksheets/WorksheetOverview.js
@@ -13,6 +13,8 @@ export class WorksheetOverview extends Component {
     this.state = {
       worksheets: [],
       worksheetCreatorOpen:false,
+      worksheetSearchType:"title",
+      worksheetSearchQuery:"",
     }
     this.unsubscribe = Store.subscribe(() => {
       this.setState({worksheets: Store.getState().worksheets});
@@ -21,6 +23,21 @@ export class WorksheetOverview extends Component {
   }
   
   render () {
+    let worksheetListFiltered = [...this.state.worksheets];
+    let q = this.state.worksheetSearchQuery.toLowerCase();
+    
+    switch (this.state.worksheetSearchType) {
+      case "title":
+        worksheetListFiltered = worksheetListFiltered.filter(v => v.name.toLowerCase().includes(q));
+        break;
+      case "inputProduct":
+        worksheetListFiltered = worksheetListFiltered.filter(v => v.inputProducts.filter(v => v.product.name.toLowerCase().includes(q)).length > 0);
+        break;
+      case "outputProduct":
+        worksheetListFiltered = worksheetListFiltered.filter(v => v.outputProducts.filter(v => v.product.name.toLowerCase().includes(q)).length > 0);
+        break;
+    }
+    
     return (
       <div className="worksheet-overview">
         <div hidden={!this.state.worksheetCreatorOpen} className="popup-container" onClick={() => this.setState({worksheetCreatorOpen:false})}>
@@ -34,9 +51,18 @@ export class WorksheetOverview extends Component {
         
         <div>
           <h1>Worksheet overview</h1>
-          <button type="button" onClick={() => this.setState({worksheetCreatorOpen:true})}>Create new</button>
+          <div className="worksheet-filter-container">
+            <select name="worksheetFilterType" title="Worksheet filter type" value={this.state.worksheetSearchType} onChange={e => this.setState({worksheetSearchType:e.target.value})}>
+              <option value="title">Title</option>
+              <option value="inputProduct">Input product</option>
+              <option value="outputProduct">Output product</option>
+            </select>
+            <input placeholder="Search" type="text" value={this.state.worksheetSearchQuery} onChange={e => this.setState({worksheetSearchQuery: e.target.value})}/>
+            <div className="flex-grow-1"></div>
+            <button type="button" onClick={() => this.setState({worksheetCreatorOpen:true})}>Create new</button>
+          </div>
           <div>
-            {this.state.worksheets.map(v => <div key={v.name} className="worksheetItem"><WorksheetItem data={v} id={v.id}></WorksheetItem></div>)}
+            {worksheetListFiltered.map(v => <div key={v.name} className="worksheetItem"><WorksheetItem data={v} id={v.id}></WorksheetItem></div>)}
           </div>
         </div>
       </div>
