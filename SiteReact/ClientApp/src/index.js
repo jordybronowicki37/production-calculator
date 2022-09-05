1767dbd90eb3df2c33ec4e681d636ef2c1f1eafd
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-09-05 20:35:09 +0200

    Replaced browser router with hash router

--- SiteReact/ClientApp/src/index.js
+++ SiteReact/ClientApp/src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
@@ -10,9 +10,9 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
+  <HashRouter basename={baseUrl}>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   rootElement);
 
 // If you want your app to work offline and load faster, you can change
