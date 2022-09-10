f3db8fc73fde9851fad1bd3913542ae8d19b2ee0
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-09-10 14:16:26 +0200

    Added NotificationThrower.js

---
+++ SiteReact/ClientApp/src/components/notification/NotificationThrower.js
@@ -0,0 +1,27 @@
+﻿import Store from "../../dataStore/DataStore";
+
+export const throwInfoNotification = function (message) {
+  throwNotification(message, "info")
+}
+
+export const throwSuccessNotification = function (message) {
+  throwNotification(message, "success")
+}
+
+export const throwWarningNotification = function (message) {
+  throwNotification(message, "warning")
+}
+
+export const throwErrorNotification = function (message) {
+  throwNotification(message, "error")
+}
+
+const throwNotification = function (message, type) {
+  Store.dispatch({
+    type:"notification/add",
+    payload:{
+      text:message,
+      type:type,
+    }
+  });
+}
