9e19c64b2bffac729b72941bb6f89754b089a9ad
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-09-28 16:07:50 +0200

    Added ConnectionBuilder

---
+++ productionCalculatorLib/components/connections/ConnectionBuilder.cs
@@ -0,0 +1,27 @@
+﻿using productionCalculatorLib.components.nodes.interfaces;
+using productionCalculatorLib.components.products;
+using productionCalculatorLib.components.worksheet;
+
+namespace productionCalculatorLib.components.connections;
+
+public class ConnectionBuilder
+{
+    private Worksheet _worksheet;
+    private INodeOut _nodeOut;
+    private INodeIn _nodeIn;
+    private Product _product;
+
+    public ConnectionBuilder(Worksheet worksheet, INodeOut nodeOut, INodeIn nodeIn, Product product)
+    {
+        _worksheet = worksheet;
+        _nodeOut = nodeOut;
+        _nodeIn = nodeIn;
+    }
+
+    public Connection Build()
+    {
+        var connection = new Connection(_nodeOut, _nodeIn, _product);
+        _worksheet.AddConnection(connection);
+        return connection;
+    }
+}
