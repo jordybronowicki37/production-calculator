fdc6eeeaf195b7355a293f9ca240f98eec2e66a0
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2023-07-05 17:28:51 +0200

    Reworked target icon

--- SiteReact/ClientApp/src/components/calculator/targets/ActiveTargetsIcon.js
+++ SiteReact/ClientApp/src/components/calculator/targets/ActiveTargetsIcon.js
@@ -1,26 +1,12 @@
 ﻿import "./ActiveTargetsIcon.css";
 
 export function ActiveTargetsIcon({targets, onOpenEditor}) {
-  let targetIcon;
-  
-  if (targets.length !== 0) {
-    if (targets[0].type === "ExactAmount") {
-      targetIcon = <div>
-        <i className='bx bx-arrow-to-right right-one'></i>
-        <i className='bx bx-arrow-to-left left-one'></i>
-      </div>;
-    } else if (targets[0].type === "MinAmount" || targets[0].type === "MaxAmount") {
-      targetIcon = <div>
-        <i className='bx bx-arrow-to-left right-one'></i>
-        <i className='bx bx-arrow-to-right left-one'></i>
-      </div>;
-    }
-  }
-  
   return (
-    <div className="active-target-icon" onClick={onOpenEditor}>
-      {targetIcon}
-      <i className='bx bx-target-lock'></i>
+    <div 
+      title="Edit targets"
+      className={`active-target-icon ${targets.length === 0 ? "": "active"}`} 
+      onClick={onOpenEditor}>
+      <span className="material-icons">bolt</span>
     </div>
   );
 }
