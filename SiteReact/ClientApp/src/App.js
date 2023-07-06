8095582979ff53b5d447f044ee5af102892d513e
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2023-07-06 21:39:23 +0200

    Removed calculator page

--- SiteReact/ClientApp/src/App.js
+++ SiteReact/ClientApp/src/App.js
@@ -5,7 +5,6 @@ import {Route} from "react-router-dom";
 import {HomePage} from "./pages/HomePage";
 import {Provider} from "react-redux";
 import Store from "./data/DataStore";
-import {CalculatorPage} from "./pages/CalculatorPage";
 import {ProjectsPage} from "./pages/ProjectsPage";
 import {EditorPage} from "./pages/EditorPage";
 
@@ -18,7 +17,6 @@ export default class App extends Component {
         <Layout>
           <Route exact path='/' component={HomePage}/>
           <Route path='/projects' component={ProjectsPage}/>
-          <Route path='/calculator/:id' component={CalculatorPage}/>
           <Route path='/editor/:id' component={EditorPage}/>
         </Layout>
       </Provider>
