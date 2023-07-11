b47348354f35dee1a367571179989e9a5594cd17
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2023-07-11 22:08:18 +0200

    Added entity container dto

--- ProductionCalculator.Api/Controllers/dto/projects/DtoProject.cs
+++ ProductionCalculator.Api/Controllers/dto/projects/DtoProject.cs
@@ -1,9 +1,7 @@
 ﻿using productionCalculatorLib.components.entityContainer;
 using productionCalculatorLib.components.project;
 using productionCalculatorLib.components.worksheet;
-using SiteReact.Controllers.dto.machines;
-using SiteReact.Controllers.dto.products;
-using SiteReact.Controllers.dto.recipes;
+using SiteReact.Controllers.dto.entityContainers;
 using SiteReact.Controllers.dto.worksheets;
 
 namespace SiteReact.Controllers.dto.projects;
@@ -12,20 +10,14 @@ public class DtoProject
 {
     public Guid Id { get; }
     public string Name { get; }
-    public Guid EntityContainerId { get; }
     public IEnumerable<DtoWorksheet> Worksheets { get; }
-    public IEnumerable<DtoProduct> Products { get; }
-    public IEnumerable<DtoRecipe> Recipes { get; }
-    public IEnumerable<DtoMachine> Machines { get; }
+    public DtoEntityContainer EntityContainer { get; }
 
     public DtoProject(Project project, EntityContainer entityContainer, IEnumerable<Worksheet> worksheets)
     {
         Id = project.Id;
         Name = project.Name;
-        EntityContainerId = project.EntityContainerId;
         Worksheets = worksheets.Select(w => new DtoWorksheet(w));
-        Products = entityContainer.Products.Select(p => new DtoProduct(p));
-        Recipes = entityContainer.Recipes.Select(r => new DtoRecipe(r));
-        Machines = entityContainer.Machines.Select(m => new DtoMachine(m));
+        EntityContainer = new DtoEntityContainer(entityContainer);
     }
 }
