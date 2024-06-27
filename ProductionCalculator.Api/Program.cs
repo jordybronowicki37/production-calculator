269b1c2b962c0a2dc1ed4eb531db6bc2799917fa
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-06-27 22:33:22 +0200

    Added EF and MySQL, also refactored all configuration logic to startup class

--- ProductionCalculator.Api/Program.cs
+++ ProductionCalculator.Api/Program.cs
@@ -1,65 +1,10 @@
-using SiteReact.Data.DbContexts;
-using SiteReact.Data.Initializers;
+using SiteReact;
 
 var builder = WebApplication.CreateBuilder(args);
 
-const string _myAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: _myAllowSpecificOrigins,
-        policy =>
-        {
-            policy.AllowAnyOrigin();
-            policy.AllowAnyMethod();
-            policy.AllowAnyHeader();
-        });
-});
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var mongoConnectionString = Environment.GetEnvironmentVariable("MongoConnectionString") ?? throw 
-    new ArgumentNullException($"Environment variable not set: MongoConnectionString");
-builder.Services.AddSingleton(new DocumentContext(mongoConnectionString));
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-else
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
-}
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var documentContext = services.GetRequiredService<DocumentContext>();
-
-    if (app.Environment.IsDevelopment())
-    {
-        TestDataInitializer.InitializeAllData(documentContext);
-    }
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseCors(_myAllowSpecificOrigins);
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");
-
+startup.Configure(app);
 app.Run();
