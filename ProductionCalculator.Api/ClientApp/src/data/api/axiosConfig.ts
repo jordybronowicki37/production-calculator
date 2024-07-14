0ad10c305d1079b1a6da86e9068aad709c525f51
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-07-14 12:55:51 +0200

    Added axios

---
+++ ProductionCalculator.Api/ClientApp/src/data/api/axiosConfig.ts
@@ -0,0 +1,30 @@
+﻿// axiosConfig.ts
+import axios, {AxiosInstance} from 'axios';
+
+function getToken(): string | null {
+  return localStorage.getItem('userToken');
+}
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: 'https://localhost:44496',
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    if (config.headers) {
+      if (config.headers.noAuth) {
+        delete config.headers.noAuth;
+      }
+      const token = getToken();
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
